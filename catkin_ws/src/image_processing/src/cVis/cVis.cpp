3320d07c4c756e66685beabb4fa3de2ed3587b0e
Chiel Bruin <chielbruin@tele2.nl>
2016-10-25 18:58:35 +0200

    Added cVis Node framework
    - The cVis node is fully functional, but the back-end is missing
    - Added CucumberContainer class to hold data

---
+++ catkin_ws/src/image_processing/src/cVis/cVis.cpp
@@ -0,0 +1,10 @@
+#include "../cucumberContainer.cpp"
+
+#include "sensor_msgs/Image.h"
+
+std::vector<CucumberContainer> processImage(sensor_msgs::Image image) {
+	std::vector<CucumberContainer> res;
+	//TODO
+	
+	return res;
+}
---
+++ catkin_ws/src/image_processing/src/cucumberContainer.cpp
@@ -0,0 +1,57 @@
+#ifndef CucumberContainer_C
+#define CucumberContainer_C
+
+#include "cucumber_msgs/Cucumber.h"
+
+/**
+ * Container class that stores a cucumbers data.
+ */
+class CucumberContainer {
+	float x, y, z;
+	
+	float width;
+	float height;
+	
+	float curvature;
+
+ public:
+	/**
+	 * Constructor for a 2D instance of the container object.
+	 */
+	CucumberContainer(float x, float y, float width, float height, float curvature) {
+		this->x = x;
+		this->y = y;
+		this->z = 0;
+		this->width = width;
+		this->height = height;
+		this->curvature = curvature;
+	}
+	
+	/**
+	 * Constructor for a 3D instance of the container object.
+	 */
+	CucumberContainer(float x, float y, float z, float width, float height, float curvature) {
+		this->x = x;
+		this->y = y;
+		this->z = z;
+		this->width = width;
+		this->height = height;
+		this->curvature = curvature;
+	}
+	
+	/**
+	 * Converts the cucumber to a cucumber_msgs::Cucumber message.
+	 * It leaves the header and dimension empty.
+	 */
+	cucumber_msgs::Cucumber toMessage() {
+		cucumber_msgs::Cucumber msg;
+		 msg.stem_position.x = x;
+		 msg.stem_position.y = y;
+		 msg.stem_position.z = z;
+		 msg.width = width;
+		 msg.height = height;
+		 msg.curvature = curvature;
+		return msg;		 
+	}
+};
+#endif
