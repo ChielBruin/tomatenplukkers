9922ee11d3bf41f3fca91fb9a0524883bd470f85
Chiel Bruin <chielbruin@tele2.nl>
2016-10-25 19:51:59 +0200

    Added IdCB node framework
    - ROS components fully functional, but the back-end is missing

---
+++ catkin_ws/src/image_processing/src/IdCB/node_IdCB.cpp
@@ -0,0 +1,37 @@
+#include "IdCB.cpp"
+
+#include "ros/ros.h"
+#include "cucumber_msgs/Cucumber.h"
+#include "sensor_msgs/PointCloud2.h"
+
+using namespace ros;
+
+Publisher cucumber_pub;
+
+void left_imageCallback(const cucumber_msgs::Cucumber& c) {
+	CucumberContainer res = to3D(c, CAM_LEFT, getPointCloud(c.header.stamp));
+	cucumber_pub.publish(res.toMessage());
+}
+
+void right_imageCallback(const cucumber_msgs::Cucumber& c) {
+	CucumberContainer res = to3D(c, CAM_RIGHT, getPointCloud(c.header.stamp));
+	cucumber_pub.publish(res.toMessage());
+}
+
+void pointCloudCallback(const sensor_msgs::PointCloud2& msg) {
+	setPointCloud(msg);
+}
+
+int main(int argc, char **argv) {
+	init(argc, argv, "IdCB");
+	ROS_INFO("IdCB started");
+
+	NodeHandle n;
+	cucumber_pub = n.advertise<cucumber_msgs::Cucumber>("stereo/cucumber", 20);
+	Subscriber image_left_sub = n.subscribe("left/cucumber", 1000, left_imageCallback);
+	Subscriber image_right_sub = n.subscribe("right/cucumber", 1000, right_imageCallback);
+	Subscriber pointcloud_sub = n.subscribe("depth/points", 1000, pointCloudCallback);
+
+	spin();
+	return 0;
+}
