1f26dc77dafd4309977bb383b6a79c7b877f34b3
Chiel Bruin <chielbruin@tele2.nl>
2016-11-17 14:11:49 +0100

    Improved the interface
    - Now shows all ros logging
    - contains hooks for camera images, detections and targets

--- catkin_ws/src/kpr_interface/src/interface.py
+++ catkin_ws/src/kpr_interface/src/interface.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 
 import rospy
-from Tkinter import *
+from Tkinter import Tk, Label
 from kpr_interface.srv import GetSettings
 from kpr_interface.msg import SetSetting
+
+from sensor_msgs.msg import Image
+from cucumber_msgs.msg import Cucumber
+from rosgraph_msgs.msg import Log
 
 settings_pub = rospy.Publisher('/settings/set', SetSetting, queue_size = 10)
 
 def loadSettings():
-	rospy.loginfo('[GUI] Waiting for setting service...')
+	rospy.loginfo('Waiting for setting service...')
 	rospy.wait_for_service('settings/getAll')
 	try:
 		resp = rospy.ServiceProxy('settings/getAll', GetSettings)()
@@ -16,7 +20,7 @@ def loadSettings():
 		
 		for setting in resp.settings:
 			settings[setting.key] = setting.value
-		rospy.loginfo('[GUI] Settings received')
+		rospy.loginfo('Settings received')
 		
 		print settings
 		return settings
@@ -31,13 +35,32 @@ def buildScreen(root, settings):
 	for setting in settings:
 		Label(root, text="{}: {}".format(setting, settings[setting])).pack()
 	
+def imageCallback(msg, root):
+	a = 0
+	
+def targetCallback(msg, root):
+	a = 0
+	
+def detectionCallback(msg, root):
+	a = 0
+	
+def diagnosticCallback(msg, log):
+	log['text'] += '\n[{}] {}'.format(msg.file, msg.msg)
+	
 if __name__ == '__main__':
 	rospy.init_node('interface')
-	settings = loadSettings()
-	
 	root = Tk()
+	
+	settings = loadSettings()
+	image_sub = rospy.Subscriber('/left/image_raw', Image, lambda msg: imageCallback(msg, root))
+	#target_sub = rospy.Subscriber('<TODO>', Cucumber, lambda msg: targetCallback(msg, root))
+	detect_sub = rospy.Subscriber('/stereo/cucumber', Cucumber, lambda msg: detectionCallback(msg, root))
+	log = Label(root, text="Log:")
+	log.pack()
+	diagnostic_sub = rospy.Subscriber('/rosout', Log, lambda msg: diagnosticCallback(msg, log))
+	
 	buildScreen(root, settings)
-	rospy.loginfo('[GUI] started')
+	rospy.loginfo('started')
 	# rospy.spin() is not needed in this case
 	root.mainloop()
-	rospy.loginfo('[GUI] stopped')
+	rospy.loginfo('stopped')
