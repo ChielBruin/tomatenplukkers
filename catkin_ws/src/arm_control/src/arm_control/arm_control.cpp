5e4899876bdea877930f27aadafcac9a128f772d
= <max_groenenboom@hotmail.com>
2016-11-29 14:29:46 +0100

    Added basic implementation of moveit to enable arm movements.

--- catkin_ws/src/arm_control/src/arm_control/arm_control.cpp
+++ catkin_ws/src/arm_control/src/arm_control/arm_control.cpp
@@ -3,11 +3,54 @@
 #include "cucumber_msgs/CucumberContainer.h"
 #include "geometry_msgs/Pose.h"
 
+#include <moveit/move_group_interface/move_group.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/planning_interface/planning_interface.h>
+#include <moveit/kinematic_constraints/utils.h>
+
+const std::string FRAME_NAME = "gripper";
+const std::string MOVE_GROUP_NAME = "gripper";
+const std::string CONSTRAINT_LINK_NAME = "?";
+
 using namespace ros;
 
-bool moveArmTo(geometry_msgs::Pose targetPos) {
-	//TODO Code to move arm to cucumber.
-	return false;
+/**
+ * Moves the arm to a certain pose.
+ * 
+ * @param [in] planningInstance The instance of the planner manager to use.
+ * @param [in] scene The instance of the planning scene to use.
+ * @param [in] targetPose The target pose at the end of the action.
+ * 
+ * @return True if the movement succeeded, false otherwise.
+ */
+bool moveArmTo(planning_interface::PlannerManagerPtr planningInstance, planning_scene::PlanningScenePtr scene, geometry_msgs::Pose targetPose) {
+	// Create messages for moveit.
+	planning_interface::MotionPlanRequest req;
+	planning_interface::MotionPlanResponse res;
+
+	// Create PoseStamped.
+	geometry_msgs::PoseStamped poseMsg;
+	poseMsg.pose = targetPose;
+	poseMsg.header.frame_id = FRAME_NAME;
+
+	// Precision tolerance of the resulting pose. Fine tune if necessary.
+	double tolerancePose = 0.01;
+	double toleranceAngle = 0.01;
+
+	// Construct movement constraints.
+	req.group_name = MOVE_GROUP_NAME;
+	moveit_msgs::Constraints poseGoal = kinematic_constraints::constructGoalConstraints(CONSTRAINT_LINK_NAME, poseMsg, tolerancePose, toleranceAngle);
+	req.goal_constraints.push_back(poseGoal);
+
+	// Do the actual planning.
+	planning_interface::PlanningContextPtr context = planningInstance->getPlanningContext(scene, req, res.error_code_);
+	context->solve(res);
+	if (res.error_code_.val != res.error_code_.SUCCESS) {
+		ROS_ERROR("Could not compute plan successfully");
+		return false;
+	}
+	return true;
 }
 
 bool startGrip(CucumberContainer cucumber) {
--- catkin_ws/src/arm_control/src/arm_control/node_arm_control.cpp
+++ catkin_ws/src/arm_control/src/arm_control/node_arm_control.cpp
@@ -1,19 +1,32 @@
 #include "arm_control.cpp"
-
 #include "ros/ros.h"
-#include "geometry_msgs/Pose.h"
-#include "cucumber_msgs/HarvestAction.h"
 
+#include "cucumber_msgs/HarvestAction.h"
 #include "cucumber_msgs/Cucumber.h"
 #include "cucumber_msgs/CucumberContainer.h"
+#include "geometry_msgs/Pose.h"
+
+#include "pluginlib/class_loader.h"
+
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/planning_interface/planning_interface.h>
+#include <moveit/kinematic_constraints/utils.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+#include <moveit_msgs/PlanningScene.h>
+
+#include <boost/scoped_ptr.hpp>
 
 using namespace ros;
+
+planning_scene::PlanningScenePtr scene;
+planning_interface::PlannerManagerPtr planner;
 
 bool getCucumber(cucumber_msgs::HarvestAction::Request &msg, cucumber_msgs::HarvestAction::Response &response) {
 	bool success = true;
 
 	CucumberContainer target = CucumberContainer(msg.cucumber);
-	success = moveArmTo(target.createPose());
+	success = moveArmTo(planner, scene, target.createPose());
 	if (!success) {
 		response.status = response.MOVE_ERR;
 		return success;
@@ -27,7 +40,7 @@ bool getCucumber(cucumber_msgs::HarvestAction::Request &msg, cucumber_msgs::Harv
 
 	cut();
 
-	success = moveArmTo(msg.dropLocation);
+	success = moveArmTo(planner, scene, msg.dropLocation);
 	if (!success) {
 		response.status = response.MOVE_ERR;
 		return success;
@@ -42,12 +55,49 @@ bool getCucumber(cucumber_msgs::HarvestAction::Request &msg, cucumber_msgs::Harv
 	return true;
 }
 
+void setupRos(NodeHandle n) {
+	robot_model_loader::RobotModelLoader robotModelLoader("robot_description");
+	robot_model::RobotModelPtr robotModel = robotModelLoader.getModel();
+	
+	planning_scene::PlanningScenePtr planningScene(new planning_scene::PlanningScene(robotModel));
+	boost::scoped_ptr<pluginlib::ClassLoader<planning_interface::PlannerManager> > plannerPluginLoader;
+	planning_interface::PlannerManagerPtr plannerInstance;
+	std::string plannerPluginName;
+	scene = planningScene;
+	planner = plannerInstance;
+	
+	if (!n.getParam("planning_plugin", plannerPluginName)) {
+		ROS_FATAL_STREAM("Could not find planner plugin name");
+	}
+	try {
+		plannerPluginLoader.reset(new pluginlib::ClassLoader<planning_interface::PlannerManager>("moveit_core", "planning_interface::PlannerManager"));
+	} catch (pluginlib::PluginlibException& e) {
+		ROS_FATAL_STREAM("Exception while creating planning plugin loader " << e.what());
+	}
+	try {
+		plannerInstance.reset(plannerPluginLoader->createUnmanagedInstance(plannerPluginName));
+		if (!plannerInstance->initialize(robotModel, n.getNamespace())) {
+			ROS_FATAL_STREAM("Could not initialize planner instance");
+		}
+		ROS_INFO_STREAM("Using planning interface '" << plannerInstance->getDescription() << "'");
+	} catch (pluginlib::PluginlibException& e) {
+		const std::vector<std::string> &classes = plannerPluginLoader->getDeclaredClasses();
+		std::stringstream sstream;
+		for (std::size_t i=0; i < classes.size(); i++) {
+			sstream << classes[i] << " ";
+		}
+		ROS_ERROR_STREAM("Exception while loading planner '" << plannerPluginName << ":" << e.what() << std::endl << "Available plugins: " << sstream.str());
+	}
+}
+
 int main(int argc, char **argv) {
 	init(argc, argv, "Arm control");
 	ROS_INFO("Arm control started");
 	
 	NodeHandle n;
 	ServiceServer cucumberService = n.advertiseService("target/cucumber", getCucumber);
+	
+	setupRos(n);
 	
 	spin();
 	return 0;
