aca254891e91731e23d71efba32e7b52a35894c1
Pascalle Wijntjes <pascalle1994@hotmail.com>
2016-11-29 13:39:11 +0100

    Fixed return values

--- catkin_ws/src/kpr_testing/src/valves.py
+++ catkin_ws/src/kpr_testing/src/valves.py
@@ -5,8 +5,20 @@ from ur_msgs.srv import *
 
 def request(req):
 	print "%i %i %f" %(req.fun, req.pin, req.state)
-	response = bool(raw_input("response (True/False) ="))
-	return SetIOResponse(response)
+	res = response()	
+	return SetIOResponse(res)
+
+def response():
+	try:
+		res = int(input("response (1/0) ="))
+		if res == 1 or res == 0:
+			return bool(res)
+		else:
+			print "Enter a 1 or 0"
+			return response()
+	except NameError:		
+		print "Not a number"
+		return response()
 
 
 rospy.init_node('io_server')
