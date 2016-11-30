d018bc0e8ec60822f61b386a0f4f63c1435862d5
Chiel Bruin <chielbruin@tele2.nl>
2016-11-30 12:11:31 +0100

    Changed from pointcloud to disparity image
    As we now need to us de stereo_image_proc node, we can use the disparity image.
    This maks the conversions way simpler. Note that we still need to do some filtering to fix holes in the image

--- catkin_ws/src/image_processing/src/IdCB/IdCB.cpp
+++ catkin_ws/src/image_processing/src/IdCB/IdCB.cpp
@@ -1,19 +1,19 @@
 #include "cucumber_msgs/CucumberContainer.h"
 
 #include "ros/ros.h"
-#include "sensor_msgs/PointCloud2.h"
+#include "stereo_msgs/DisparityImage.h"
 #include "cucumber_msgs/Cucumber.h"
 #include "std_msgs/Time.h"
 
 #define CAM_LEFT 0
 #define CAM_RIGHT 1
 
-sensor_msgs::PointCloud2 pointCloud;
+stereo_msgs::DisparityImage disparity;
 
 /**
- * Calculate the 3D properties of the cucumber from the given camera position and pointcloud.
+ * Calculate the 3D properties of the cucumber from the given camera position and disparity image.
  */
-CucumberContainer to3D(cucumber_msgs::Cucumber in, int camera, sensor_msgs::PointCloud2 pointcloud) {
+CucumberContainer to3D(cucumber_msgs::Cucumber in, int camera, stereo_msgs::DisparityImage pointcloud) {
 	CucumberContainer tmp = CucumberContainer(in.stem_position.x, in.stem_position.y, in.width, in.height, in.curvature);
 	if(camera == CAM_LEFT) {
 		return tmp;
@@ -23,18 +23,18 @@ CucumberContainer to3D(cucumber_msgs::Cucumber in, int camera, sensor_msgs::Poin
 }
 
 /**
- * Add a point cloud to the set.
- * This method also trims all the old point clouds on update.
+ * Add a disparity image to the set.
+ * This method also trims all the images on update.
  */
-void setPointCloud(sensor_msgs::PointCloud2 newPC) {
-	pointCloud = newPC;
+void setDisparityImage(stereo_msgs::DisparityImage newDisp) {
+	disparity = newDisp;
 }
 
 /**
- * Get the point cloud that corresponds with the given timestamp.
- * Returns a null-pointer when the point cloud cannot be found.
+ * Get the disparity image that corresponds with the given timestamp.
+ * Returns a null-pointer when the disparity cannot be found.
  */
-sensor_msgs::PointCloud2 getPointCloud(ros::Time timestamp) {
-	return pointCloud;
+stereo_msgs::DisparityImage getDisparityImage(ros::Time timestamp) {
+	return disparity;
 	// TODO time sync
 }
