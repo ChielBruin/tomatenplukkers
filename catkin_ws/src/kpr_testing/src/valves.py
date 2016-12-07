e99a8b31b699f56df068a2155221ae65efb45b5c
Pascalle Wijntjes <pascalle1994@hotmail.com>
2016-12-07 14:29:36 +0100

    Added keyboard interactions +cleaned/deleted some code +input pins added

--- catkin_ws/src/kpr_testing/src/valves.py
+++ catkin_ws/src/kpr_testing/src/valves.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python
 
 import rospy, termios, sys, contextlib, os
-from distutils.util import strtobool
+import numpy as np
 from ur_msgs.srv import *
 from ur_msgs.msg import *
 
 header = """
 Check the status of I/O-pins
 \t-^D to exit
-\t-t to toggle service succes
+\t-i to change income settings
 \t-p to send io states
+\t-t to toggle service succes
 """
 
 def display():
 	os.system('clear')
 	print header
 #Making table
-	a = "Pin     |  State     Fun     "
-	print a
-	print "-"*len(a)
+	tableHeader = "Pin     |  State     Fun     "
+	print tableHeader
+	print "-"*len(tableHeader)
 	print "VacSens | ", info[VacSens][0], "  ", info[VacSens][1]
 	print "Gripper | ", info[Gripper][0], "  ", info[Gripper][1]
 
@@ -26,20 +27,13 @@ def display():
 VacSens = "DI1"
 Gripper = "DO2"
 
+#Set succes response
 response = True
 
 #dictionary of pins
 info = {VacSens:["-","     -"], Gripper:["-","     -"]}
 
-#Checking if used pins are correct
-def pin_check(pin):
-	if (pin == VacSens) or (pin == Gripper):
-		print "correct io pins used"
-		return True
-	else:
-		print "incorrect io pins used"
-		return False
-
+#needed for the "pressing keys"
 @contextlib.contextmanager
 def raw_mode(file):
     old_attrs = termios.tcgetattr(file.fileno())
@@ -54,6 +48,7 @@ def raw_mode(file):
 def request(req):
 	global response
 	display()
+#Checking if changes wil be made to used pins 
 	if req.fun == 1:
 		pin = 'DO' + str(req.pin)
 	elif req.fun == 4:
@@ -61,21 +56,16 @@ def request(req):
 	else:
 		rospy.logerr("Writing to unused pin")
 		return SetIOResponse(False)
-#Change Dictionary "info" with correct data
 	if pin not in info:
 		rospy.logerr("Writing to unused pin")
 		return SetIOResponse(False)
+#Change Dictionary "info" with correct data
 	info[pin][0] = req.state
 	if req.fun == 1:
 		info[pin][1] = "Digital Out"
 	else:
 		info[pin][1] = "Analog Out"
-	print info
-	print "fun is %i" %(req.fun)
-	print "pin is %i" %(req.pin)
-	print "state is %f \n" %(req.state)
-#prints if the correct pins are used
-	print pin_check(pin)
+
 	display()
 	res = response	
 	return SetIOResponse(res)
@@ -84,20 +74,71 @@ def get_message():
 	msg = IOStates()
 	for pin in info:
 		if info[pin][1] == "Digital Out":
-			msg.digital_out_states.append(Digital(pin, bool(info[pin][0])))
+			msg.digital_out_states.append(Digital(int(pin[2]), bool(info[pin][0])))
 		elif info[pin][1] == "Analog Out":
-			msg.analog_out_states.append(Analog(pin, info[pin][0]))
+			msg.analog_out_states.append(Analog(int(pin[2]), float(info[pin][0])))
+		elif info[pin][1] == "Digital In":
+			msg.digital_in_states.append(Digital(int(pin[2]), bool(info[pin][0])))
+		elif info[pin][1] == "Analog In":
+			msg.analog_in_states.append(Analog(int(pin[2]), float(info[pin][0])))
 	return msg
 
 def processKey(key, pub):
 	global response
-	if key == chr(116):
+	if key == chr(116): #t
 		response = not response
+		display()
+		print "Your response is now: ", response
 		return
-	if 	key == chr(112):
+	if key == chr(112): #p
 		pub.publish(get_message())
+		display()
+		print "you have published the data"
+		return
+	if key == chr(105): #i
+		display()
+		print "Changing the input settings"
+		income()
 		return
 	rospy.logwarn('unknown function %s', key)
+
+def income():
+	try:
+		fun = int(raw_input("What is the fun? "))
+		if fun != 5 or fun != 6:
+			display()
+			rospy.logerr("Fun nr not available")
+			return SetIOResponse(False)
+		print fun
+	except ValueError:
+		rospy.logerr("Not a Number")
+		return SetIOResponse(False)
+
+	pinQ = str(raw_input("Which pin you want to set? " ))
+	print pinQ
+
+	if fun == 5:
+		pin = 'DI' + pinQ
+	elif fun == 6:
+		pin = 'AI' + pinQ
+	else:
+		rospy.logerr("Writing to unused pin")
+		return SetIOResponse(False)
+#Checks if pin in dictionary
+	if pin not in info:
+		rospy.logerr("Writing to unused pin")
+		return SetIOResponse(False)
+	print "writing to pin ", pin
+	state = float(raw_input("What is the state? "))
+#Send state to dictionary 
+	info[pin][0] = state 
+#Send fun to dictionary
+	if fun == 5: #fun
+		info[pin][1] = "Digital In"
+	else:
+		info[pin][1] = "Analog In"
+	display()
+	return 
 
 def main():
 	rospy.init_node('io_server')
