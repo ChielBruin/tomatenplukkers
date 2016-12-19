9ceee9ea9e4df1cb24755ecdb5bef5e92e45e513
= <max_groenenboom@hotmail.com>
2016-12-19 11:53:41 +0100

    Added table to planning scene. Also added rviz to testing launch and optional to real launch.

--- catkin_ws/src/kpr_arm_control/src/arm_control/node_arm_control.cpp
+++ catkin_ws/src/kpr_arm_control/src/arm_control/node_arm_control.cpp
@@ -5,18 +5,24 @@
 #include "cucumber_msgs/HarvestAction.h"
 #include "cucumber_msgs/Cucumber.h"
 #include "cucumber_msgs/CucumberContainer.h"
-#include "geometry_msgs/Pose.h"
-#include "geometry_msgs/Quaternion.h"
 
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Quaternion.h>
 #include <ur_msgs/SetIO.h>
+#include <shape_msgs/SolidPrimitive.h>
+#include <moveit_msgs/PlanningScene.h>
 
 #include <moveit/move_group_interface/move_group.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/planning_scene/planning_scene.h>
 
 using namespace ros;
 
 const std::string NODE_NAME = "Arm Control";
 const std::string move_group_name("manipulator");
+
+Publisher planning_scene_diff_publisher;
 
 /**
  * Attempts to pick a cucumber. This function does the following things:
@@ -100,6 +106,39 @@ void setupMoveIt(NodeHandle n) {
 
 	ROS_INFO("Reference frame: %s", move_group_ptr->getPlanningFrame().c_str());
 	ROS_INFO("Reference frame: %s", move_group_ptr->getEndEffectorLink().c_str());
+
+	planning_scene_diff_publisher = n.advertise<moveit_msgs::PlanningScene>("planning_scene", 1);
+
+	moveit_msgs::AttachedCollisionObject attached_object;
+	attached_object.link_name = "base_link";
+	attached_object.object.header.frame_id = "table_attach";
+	attached_object.object.id = "table";
+
+	geometry_msgs::Pose pose;
+	pose.position.z = -0.05;
+	pose.orientation.w = 1.0;
+
+	shape_msgs::SolidPrimitive primitive;
+	primitive.type = primitive.BOX;
+	primitive.dimensions.resize(3);
+	primitive.dimensions[0] = 1.5;
+	primitive.dimensions[1] = 1;
+	primitive.dimensions[2] = 0.1;
+
+	attached_object.object.primitives.push_back(primitive);
+	attached_object.object.primitive_poses.push_back(pose);
+	attached_object.object.operation = attached_object.object.ADD;
+
+	// Wait until the publisher is connected.
+	WallDuration sleep_t(0.1);
+	while(planning_scene_diff_publisher.getNumSubscribers() < 1) {
+		sleep_t.sleep();
+	}
+
+	moveit_msgs::PlanningScene planning_scene;
+	planning_scene.world.collision_objects.push_back(attached_object.object);
+	planning_scene.is_diff = true;
+	planning_scene_diff_publisher.publish(planning_scene);
 }
 
 /**
