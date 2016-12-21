2123bd7c196be1ffccd3c7dc9233a9680557f6cd
Chiel Bruin <chielbruin@tele2.nl>
2016-12-21 11:44:31 +0100

    Added io functions

--- catkin_ws/src/kpr_arm_control/src/py_arm_control/arm_control.py
+++ catkin_ws/src/kpr_arm_control/src/py_arm_control/arm_control.py
@@ -9,6 +9,8 @@ import smach_ros
 from cucumber_msgs.srv import HarvestAction, HarvestActionResponse
 from geometry_msgs.msg import Pose
 from moveit_msgs.msg import AttachedCollisionObject
+from ur_msgs.msg import IOStates
+from ur_msgs.srv import SetIO
 
 import states as state
 import sceneObjects as sceneObj
@@ -89,11 +91,41 @@ def setupMoveIt():
 	group = moveit_commander.MoveGroupCommander("manipulator")
 	return (robot, scene, group)
 
+def IOStatesCallback(msg):
+	global analogPinStates, digitalPinStates
+	
+	for pin in msg.digital_out_states:
+		digitalPinStates[pin.pin] = pin.state
+		
+	for pin in msg.analog_out_states:
+		analogPinStates[pin.pin] = pin.state
+	
+def setIO(pin, value):
+	try:
+		set_io = rospy.ServiceProxy('set_io', SetIO)
+		return set_io(SetIORequest.FUN_SET_DIGITAL_OUT, pin, value)
+	except rospy.ServiceException, e:
+		print "Service call failed: %s"%e
+		return False
+
+def getAnalog(pin):
+	global analogPinStates
+	return analogPinStates[pin]
+
+def getDigital(pin):
+	global digitalPinStates
+	return digitalPinStates[pin]
+	
+def setupIO():
+	rospy.wait_for_service('set_io')
+	io_states_sub = rospy.Subscriber("io_states", IOStates, IOStatesCallback);
+	
 if __name__ == '__main__':
 	rospy.init_node('ArmControl')
 	s = rospy.Service('target/cucumber', HarvestAction, getCucumberCallback)
 	aco_pub = rospy.Publisher('attached_collision_object', AttachedCollisionObject, queue_size=10)
 	(robot, scene, group) = setupMoveIt()
+	setupIO()
 	startingPosition = group.get_current_pose()
 	rospy.loginfo("Started")
 	addSceneObjects(aco_pub)
