5fa15248a35cfb6c4861b2eb71cdedeb93b5fc6b
Chiel Bruin <chielbruin@tele2.nl>
2016-12-22 17:53:54 +0100

    Made the suggested fixes

--- catkin_ws/src/kpr_arm_control/src/py_arm_control/arm_control.py
+++ catkin_ws/src/kpr_arm_control/src/py_arm_control/arm_control.py
@@ -72,12 +72,12 @@ Create a fresh state machine.
 	return sm
 
 def moveArmTo(pose_target):
-'''
-Plan the movement to the secified position and move the arm.
+	'''
+	Plan the movement to the specified position and move the arm.
 
-@param pose_target: The Pose of the goal posisiton
-@return True when the move succeded, False otherwise
-'''
+	@param pose_target: The Pose of the goal posisiton
+	@return True when the move succeded, False otherwise
+	'''
 	global group, robot
 	group.set_start_state_to_current_state()
 	group.clear_pose_targets()
@@ -87,34 +87,34 @@ Plan the movement to the secified position and move the arm.
 	return group.go(wait=True)
 	 
 def getCucumberCallback (req):
-'''
-Callback for the cucumber targets.
-Starts the state machine with the provided data
+	'''
+	Callback for the cucumber targets.
+	Starts the state machine with the provided data
 
-@param req: The HarvestAction request sent
-@return A HarvestActionResponse with the success codes
-'''
+	@param req: The HarvestAction request sent
+	@return A HarvestActionResponse with the success codes
+	'''
 	sm = createStateMachine()
 	sm.userdata.request = req
 	outcome = sm.execute()	
 	return HarvestActionResponse(success[outcome])
 
 def addSceneObjects(aco_publisher):
-'''
-Add the table and endeffector to the planning scene.
+	'''
+	Add the table and endeffector to the planning scene.
 
-@param aco_publisher: The publisher for the scene objects
-'''
+	@param aco_publisher: The publisher for the scene objects
+	'''
 	global group
 	aco_publisher.publish(sceneObj.table())
 	aco_publisher.publish(sceneObj.endEffector(group))
 	
 def setupMoveIt():
-'''
-Initialize the moveIt! framework
+	'''
+	Initialize the moveIt! framework
 
-@return A triplet containing the robot- and group commander and the planning scene
-'''
+	@return A triplet containing the robot- and group commander and the planning scene
+	'''
 	moveit_commander.roscpp_initialize(sys.argv)
 	group = moveit_commander.MoveGroupCommander("manipulator")
 	robot = moveit_commander.RobotCommander()
@@ -122,10 +122,10 @@ Initialize the moveIt! framework
 	return (robot, scene, group)
 
 def IOStatesCallback(msg):
-'''
-Callback method for receiving io states from the arm.
-Stores the received states.
-'''
+	'''
+	Callback method for receiving io states from the arm.
+	Stores the received states.
+	'''
 	global analogPinStates, digitalPinStates
 	for pin in msg.digital_in_states:
 		digitalPinStates[pin.pin] = pin.state
@@ -134,14 +134,14 @@ Stores the received states.
 		analogPinStates[pin.pin] = pin.state
 	
 def setIO(pin, value):
-'''
-Set the Value of a specified IO pin.
-Note that it only writes to digital pins as we currently are not using the analog pins.
+	'''
+	Set the Value of a specified IO pin.
+	Note that it only writes to digital pins as we currently are not using the analog pins.
 
-@param pin: The pin to write to
-@param value: The value that is written to the pin
-@return True when the value is set correctly, False otherwise
-'''
+	@param pin: The pin to write to
+	@param value: The value that is written to the pin
+	@return True when the value is set correctly, False otherwise
+	'''
 	try:
 		set_io = rospy.ServiceProxy('set_io', SetIO)
 		#set_io = rospy.ServiceProxy('set_io_testing', SetIO)	# Use these lines when testing using robot.launch
@@ -151,30 +151,30 @@ Note that it only writes to digital pins as we currently are not using the analo
 		return False
 
 def getAnalog(pin):
-'''
-Get the value of the specified analog pin
+	'''
+	Get the value of the specified analog pin
 
-@param pin: The pin to get the value of
-@return The analog value of the pin
-'''
+	@param pin: The pin to get the value of
+	@return The analog value of the pin
+	'''
 	global analogPinStates
 	return analogPinStates[pin]
 
 def getDigital(pin):
-'''
-Get the value of the specified digital pin
+	'''
+	Get the value of the specified digital pin
 
-@param pin: The pin to get the value of
-@return The digital value of the pin (True/False)
-'''
+	@param pin: The pin to get the value of
+	@return The digital value of the pin (True/False)
+	'''
 	global digitalPinStates
 	return digitalPinStates[pin]
 	
 def setupIO():
-'''
-Setup the IO subscriber and the setIO service.
-@return The subscriber to the io_states channel
-'''
+	'''
+	Setup the IO subscriber and the setIO service.
+	@return The subscriber to the io_states channel
+	'''
 	rospy.wait_for_service('set_io')
 	io_states_sub = rospy.Subscriber("io_states", IOStates, IOStatesCallback);
 	#rospy.wait_for_service('set_io_testing')											# Use these lines when testing using robot.launch
@@ -182,16 +182,16 @@ Setup the IO subscriber and the setIO service.
 	return io_states_sub
 
 def writeWithDigitalFeedback(outPin, value, inPin, expected):
-'''
-Write a value to the specified pin and check if another pin reads an expected value.
-This last check is performed multiple times to account for slow changing values.
+	'''
+	Write a value to the specified pin and check if another pin reads an expected value.
+	This last check is performed multiple times to account for slow changing values.
 
-@param outPin: The pin to write the value to
-@param value: The value to write to the inPin
-@param inPin: The pin to read from
-@param expected: The expected value on the inPin
-@return True when writing succeeds and the expected value is read, False otherwise
-'''
+	@param outPin: The pin to write the value to
+	@param value: The value to write to the inPin
+	@param inPin: The pin to read from
+	@param expected: The expected value on the inPin
+	@return True when writing succeeds and the expected value is read, False otherwise
+	'''
 	if not setIO(outPin, value):
 		return False
 	
@@ -202,17 +202,17 @@ This last check is performed multiple times to account for slow changing values.
 	return False
 
 def writeWithAnalogFeedback(outPin, value, inPin, expected, delta):
-'''
-Write a value to the specified pin and check if another pin reads an expected value.
-This last check is performed multiple times to account for slow changing values.
+	'''
+	Write a value to the specified pin and check if another pin reads an expected value.
+	This last check is performed multiple times to account for slow changing values.
 
-@param outPin: The pin to write the value to
-@param value: The value to write to the inPin
-@param inPin: The pin to read from
-@param expected: The expected value on the inPin
-@param delta: The maximum difference between the read and expected value to be considered equal
-@return True when writing succeeds and the expected value is read, False otherwise
-'''
+	@param outPin: The pin to write the value to
+	@param value: The value to write to the inPin
+	@param inPin: The pin to read from
+	@param expected: The expected value on the inPin
+	@param delta: The maximum difference between the read and expected value to be considered equal
+	@return True when writing succeeds and the expected value is read, False otherwise
+	'''
 	if not setIO(outPin, value):
 		return False
 	
@@ -223,10 +223,10 @@ This last check is performed multiple times to account for slow changing values.
 	return False
 
 if __name__ == '__main__':
-'''
-Main method of arm_control.py.
-Starts moveIt! and registers subscribers/publishers.
-'''
+	'''
+	Main method of arm_control.py.
+	Starts moveIt! and registers subscribers/publishers.
+	'''
 	rospy.init_node('ArmControl')
 	s = rospy.Service('target/cucumber', HarvestAction, getCucumberCallback)
 	aco_pub = rospy.Publisher('attached_collision_object', AttachedCollisionObject, queue_size=10)
--- catkin_ws/src/kpr_arm_control/src/py_arm_control/states.py
+++ catkin_ws/src/kpr_arm_control/src/py_arm_control/states.py
@@ -23,19 +23,19 @@ VACUUM_ON = True
 VACUUM_OFF = False
 
 class MoveToCucumber(smach.State):
-'''
-State representing the movement towards the cucumber.
-'''
+	'''
+	State representing the movement towards the cucumber.
+	'''
 	def __init__(self, moveArmTo):
 		self.moveArmTo = moveArmTo
 		smach.State.__init__(self, outcomes=['MoveOK', 'MoveError'], input_keys=['data'])
 
 	def execute(self, userdata):
-	'''
-	Move the arm to the position of the cucumber and exit this state when reached
-	
-	@return 'MoveOK' when the move was successful, 'MoveError' otherwise
-	'''
+		'''
+		Move the arm to the position of the cucumber and exit this state when reached
+		
+		@return 'MoveOK' when the move was successful, 'MoveError' otherwise
+		'''
 		rospy.loginfo('Executing state MoveToCucumber')
 		pose = Pose(userdata.data.cucumber.stem_position, Quaternion(0,0,0,1))
 		# TODO: Make the last section of movement straight towards the produce
@@ -45,19 +45,19 @@ State representing the movement towards the cucumber.
 			return 'MoveError'
 
 class CloseGripper(smach.State):
-'''
-State representing the the closing of the gripper.
-'''
+	'''
+	State representing the the closing of the gripper.
+	'''
 	def __init__(self, setIO):
 		self.setIO = setIO
 		smach.State.__init__(self, outcomes=['GripperClosed', 'GripperError'])
 
 	def execute(self, userdata):
-	'''
-	Close the gripper and check if this is actualy done.
-	
-	@return 'GripperClosed' when successful, 'GripperError' otherwise
-	'''
+		'''
+		Close the gripper and check if this is actualy done.
+		
+		@return 'GripperClosed' when successful, 'GripperError' otherwise
+		'''
 		rospy.loginfo('Executing state CloseGripper')
 		if self.setIO(GRIPPER_OUT, GRIPPER_CLOSE, GRIPPER_IN, GRIPPER_CLOSE):
 			return 'GripperClosed'
@@ -65,19 +65,19 @@ State representing the the closing of the gripper.
 			return 'GripperError'
 
 class VacuumGrip(smach.State):
-'''
-State representing the gripping of the cucumber using the vacuum suction cup.
-'''
+	'''
+	State representing the gripping of the cucumber using the vacuum suction cup.
+	'''
 	def __init__(self, setIO):
 		self.setIO = setIO
 		smach.State.__init__(self, outcomes=['VacuumCreated', 'VacuumError'])
 
 	def execute(self, userdata):
-	'''
-	Enable the vacuum and make sure it is correctly initialted.
-	
-	@return 'VacuumCreated' when the vacuum is created succesfully, 'VacuumError' otherwise
-	'''
+		'''
+		Enable the vacuum and make sure it is correctly initiated.
+		
+		@return 'VacuumCreated' when the vacuum is created succesfully, 'VacuumError' otherwise
+		'''
 		rospy.loginfo('Executing state VacuumGrip')
 		if self.setIO(VACUUM_OUT, VACUUM_ON, VACUUM_IN, VACUUM_ON):
 			return 'VacuumCreated'
@@ -86,19 +86,19 @@ State representing the gripping of the cucumber using the vacuum suction cup.
 			return 'VacuumError'
 
 class Cut(smach.State):
-'''
-State representing the cutting of the stem of the cucumber.
-'''
+	'''
+	State representing the cutting of the stem of the cucumber.
+	'''
 	def __init__(self, setIO):
 		self.setIO = setIO
 		smach.State.__init__(self, outcomes=['StemCutted', 'CutterError'])
 		
 	def execute(self, userdata):
-	'''
-	Cut the stem by closing and opening the cutter.
-	
-	@return 'StemCutted' when the cutting was successful, 'CutterError' otherwise
-	'''
+		'''
+		Cut the stem by closing and opening the cutter.
+		
+		@return 'StemCutted' when the cutting was successful, 'CutterError' otherwise
+		'''
 		rospy.loginfo('Executing state Cut')
 		if self.setIO(CUTTER_OUT, CUTTER_CLOSE, CUTTER_IN, CUTTER_CLOSE):
 			if self.setIO(CUTTER_OUT, CUTTER_OPEN, CUTTER_IN, CUTTER_OPEN):
@@ -106,19 +106,19 @@ State representing the cutting of the stem of the cucumber.
 		return 'CutterError'
 
 class MoveToDropoff(smach.State):
-'''
-State representing the arm movement towards the dropoff location
-'''
+	'''
+	State representing the arm movement towards the dropoff location
+	'''
 	def __init__(self, moveArmTo):
 		self.moveArmTo = moveArmTo
 		smach.State.__init__(self, outcomes=['MoveOK', 'MoveError'], input_keys=['data'])
 
 	def execute(self, userdata):
-	'''
-	Move the arm to the dropoff location provided by the input data.
-	
-	@return 'MoveOK' when the move was successful, 'MoveError' otherwise
-	'''
+		'''
+		Move the arm to the dropoff location provided by the input data.
+		
+		@return 'MoveOK' when the move was successful, 'MoveError' otherwise
+		'''
 		rospy.loginfo('Executing state MoveToDropoff')
 		if self.moveArmTo(userdata.data.dropLocation):
 			return 'MoveOK'
@@ -126,19 +126,19 @@ State representing the arm movement towards the dropoff location
 			return 'MoveError'
 
 class OpenGripper(smach.State):
-'''
-State representing the opening of the gripper and releasing the produce.
-'''
+	'''
+	State representing the opening of the gripper and releasing the produce.
+	'''
 	def __init__(self, setIO):
 		self.setIO = setIO
 		smach.State.__init__(self, outcomes=['GripperOpened', 'GripperError'])
 
 	def execute(self, userdata):
-	'''
-	Drop the produce by opeing the gripper and removing the vacuum.
-	
-	@return 'GripperOpened' when the produce is dropped correctly, 'GripperError' otherwise
-	'''
+		'''
+		Drop the produce by opeing the gripper and removing the vacuum.
+		
+		@return 'GripperOpened' when the produce is dropped correctly, 'GripperError' otherwise
+		'''
 		rospy.loginfo('Executing state OpenGripper')
 		# TODO: Swap these according to the dropping procedure, 
 		# this order seems fine for horizontal dropping in a crate.
@@ -148,19 +148,19 @@ State representing the opening of the gripper and releasing the produce.
 		return 'GripperError'
 
 class MoveToStart(smach.State):
-'''
-State representing the movement of the arm while returning to its initial position.
-'''
+	'''
+	State representing the movement of the arm while returning to its initial position.
+	'''
 	def __init__(self, moveArmTo):
 		self.moveArmTo = moveArmTo
 		smach.State.__init__(self, outcomes=['MoveOK', 'MoveError'], input_keys=['start'])
 
 	def execute(self, userdata):
-	'''
-	Move the arm to the initial starting location provided by the input data.
-	
-	@return 'MoveOK' when the move was successful, 'MoveError' otherwise
-	'''
+		'''
+		Move the arm to the initial starting location provided by the input data.
+		
+		@return 'MoveOK' when the move was successful, 'MoveError' otherwise
+		'''
 		# TODO: This state should be executed when something goes wrong, but this needs to be discussed
 		rospy.loginfo('Executing state MoveToStart')
 		if self.moveArmTo(userdata.start):
